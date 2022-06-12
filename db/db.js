039832b04c3b3ec55ab326eeef8640a9b766f64f
Evyn <evynjenkins01@gmail.com>
2022-06-12 11:51:20 +1000

    Amend db path

--- db/db.js
+++ db/db.js
@@ -1,6 +1,10 @@
+import path from "path";
 import { open } from "sqlite";
 
 var sqlite3 = require("sqlite3").verbose();
-var db = open({ filename: "db/api_db.sqlite", driver: sqlite3.Database });
+var db = open({
+  filename: "./db/api_db.sqlite",
+  driver: sqlite3.Database,
+});
 // var db = new sqlite3.Database("db/api_db.sqlite");
 export default db;
