78f77403654b3b749670660246b3e250ea3ee2fb
Evyn <evynjenkins01@gmail.com>
2022-06-12 12:04:16 +1000

    Dummy production data

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import KoalaDashboard from "../components/dashboards/KoalaDashboard";
 import TreeDashboard from "../components/dashboards/TreeDashboard";
 import Layout from "../components/Layout";
 import db from "../db/db";
+import { dummykoalas, dummyhotspots } from "../db/DummyData";
 
 export default function Home({ hotspots, koalas }) {
   const userLocation = useState(null);
@@ -27,22 +28,29 @@ export default function Home({ hotspots, koalas }) {
 }
 
 export async function getServerSideProps(context) {
-  const hotspots = await (
-    await db
-  ).all(`
-  SELECT 
-    COUNT(number_seen) AS number_seen,
-    locality
-  FROM
-    koalas
-  GROUP BY
-    locality
-  ORDER BY
-    number_seen DESC;`);
+  try {
+    const hotspots = await (
+      await db
+    ).all(`
+    SELECT 
+      COUNT(number_seen) AS number_seen,
+      locality
+    FROM
+      koalas
+    GROUP BY
+      locality
+    ORDER BY
+      number_seen DESC;`);
 
-  const koalas = await (await db).all("SELECT * FROM koalas");
-
-  return {
-    props: { hotspots, koalas },
-  };
+    const koalas = await (await db).all("SELECT * FROM koalas");
+    return {
+      props: { hotspots, koalas },
+    };
+  } catch (error) {
+    console.log("BLAHHHH");
+    console.log(error);
+    return {
+      props: { dummyhotspots, dummykoalas },
+    };
+  }
 }
