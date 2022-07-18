c51403d6655129fb667ea08fe6d53e332bfddc73
Evyn <evynjenkins01@gmail.com>
2022-07-18 12:38:24 +1000

    Tree distance in meters

--- components/dashboards/TreeDashboard.js
+++ components/dashboards/TreeDashboard.js
@@ -3,34 +3,42 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import DataTable from "../DataTable";
 
-function d(point, origin) {
-  return Math.sqrt(
-    Math.pow(point.lat - origin.lat, 2) + Math.pow(point.lng - origin.lng, 2)
-  );
+// Distance in meters
+function measure(lat1, lon1, lat2, lon2) {
+  var R = 6378.137; // Radius of earth in KM
+  var dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+  var dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos((lat1 * Math.PI) / 180) *
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c;
+  return (d * 1000).toFixed(2); // meters
 }
 
 export default function TreeDashboard(props) {
-  var treesdata = props.treesdata.map((t) => {
-    return {
-      road: t.properties.road_name,
-      suburb: t.properties.suburb,
-      species: t.properties.tree_speci,
-      lat: t.properties.latitudey,
-      lng: t.properties.longitudex,
-    };
-  });
   if (props.userLocation[0]) {
-    treesdata = treesdata.sort(
-      (a, b) =>
-        d(
-          { lat: a.lat, lng: a.lng },
-          { lat: props.userLocation[0][0], lng: props.userLocation[0][1] }
-        ) -
-        d(
-          { lat: b.lat, lng: b.lng },
-          { lat: props.userLocation[0][0], lng: props.userLocation[0][1] }
-        )
-    );
+    var treesdata = props.treesdata.map((t) => {
+      return {
+        road: t.properties.road_name,
+        species: t.properties.tree_speci,
+        distance: measure(
+          t.properties.latitudey,
+          t.properties.longitudex,
+          props.userLocation[0][0],
+          props.userLocation[0][1]
+        ),
+      };
+    });
+
+    treesdata = treesdata.sort((a, b) => a.distance - b.distance);
+  } else {
+    var treesdata = [
+      { "No Location Found": "Press the button above to find your location" },
+    ];
   }
   return (
     <div className="w-full my-2">
